7004961867cff3d29f23c18867ae785531ca569b
Tyler Palmer <tyler_palmer@uri.edu>
2024-09-17 22:40:30 -0400

    Initial commit of the flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _displayText = '';
+  String _expression = '';
+
+  void _appendText(String text) {
+    setState(() {
+      _displayText += text;
+      _expression += text;
+    });
+  }
+
+  void _calculateResult() {
+    try {
+      final expression = Expression.parse(_expression);
+      final evaluator = const ExpressionEvaluator();
+      final result = evaluator.eval(expression, {});
+      setState(() {
+        _displayText += ' = ${result.toString()}';
+        _expression = result.toString();
+      });
+    } catch (e) {
+      setState(() {
+        _displayText = 'Error';
+      });
+    }
+  }
+
+  void _clear() {
+    setState(() {
+      _displayText = '';
+      _expression = '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Calculator - Tyler Palmer'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                _displayText,
+                style: TextStyle(
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 1),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  _buildButton('7'),
+                  _buildButton('8'),
+                  _buildButton('9'),
+                  _buildButton('/'),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('4'),
+                  _buildButton('5'),
+                  _buildButton('6'),
+                  _buildButton('*'),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('1'),
+                  _buildButton('2'),
+                  _buildButton('3'),
+                  _buildButton('-'),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('0'),
+                  _buildButton('C', isClear: true),
+                  _buildButton('='),
+                  _buildButton('+'),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String text, {bool isClear = false}) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          if (isClear) {
+            _clear();
+          } else if (text == '=') {
+            _calculateResult();
+          } else {
+            _appendText(text);
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(20.0),
+          textStyle: TextStyle(fontSize: 24.0),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
